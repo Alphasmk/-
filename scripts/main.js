c99742b0a7506ba9b2d9885ced40dfa6c62ddc10
German <Statkog@gmail.com>
2024-04-21 22:26:21 +0300

    Добавлена версия главной страницы сайта для планшета и компьютера

--- scripts/main.js
+++ scripts/main.js
@@ -4,21 +4,49 @@ $(document).ready(function(){
       url: "catalog.xml",
       dataType: "xml",
       success: function(xml){
-          $(xml).find('product').each(function(){
-              var name = $(this).find('name').text();
-              var image = $(this).find('image').text();
-              var price = $(this).find('price').text();
-              $('.slider').append('<div><img src="' + image + '"><p><h3>' + name + '</h3>' + price + '</p><a href="#" class="button">Приобрести</a></div>');
-          });
-          $('.single-item').slick({
-              infinite: true,
-              dots: false,
-              arrows: true,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              prevArrow: '<img src="images/left.png" class="slick-prev">',
-              nextArrow: '<img src="images/right.png" class="slick-next">'
-          });
+        if(window.matchMedia('(max-width: 1000px)').matches)
+          {
+            $(xml).find('product').each(function(){
+                var name = $(this).find('name').text();
+                var image = $(this).find('image').text();
+                var price = $(this).find('price').text();
+                var area = $(this).find('area').text();
+                $('.slider').append('<div style="grid-area: ' + area + '"><img src="' + image + '"><p><h3>' + name + '</h3>' + price + '</p><a href="#" class="button">Приобрести</a></div>');
+            });
+          }
+          if(window.matchMedia('(min-width: 1001px)').matches)
+          {
+            $(xml).find('product').each(function(){
+                var name = $(this).find('name').text();
+                var image = $(this).find('image').text();
+                var price = $(this).find('price').text();
+                var area = $(this).find('area').text();
+                $('.itcat').append('<div style="grid-area: ' + area + '"><img src="' + image + '"><p><h3>' + name + '</h3>' + price + '</p><a href="#" class="button">Приобрести</a></div>');
+            });
+          }
+          if(window.matchMedia('(max-width: 420px)').matches)
+          {
+            $('.single-item').slick({
+                infinite: true,
+                dots: false,
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow: '<img src="images/left.png" class="slick-prev">',
+                nextArrow: '<img src="images/right.png" class="slick-next">'
+            });
+          }
+          if (window.matchMedia('(min-width: 421px) and (max-width: 1000px)').matches) {
+            $('.single-item').slick({
+                infinite: true,
+                dots: false,
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                prevArrow: '<img src="images/left.png" class="slick-prev">',
+                nextArrow: '<img src="images/right.png" class="slick-next">'
+            });
+        }
       }
   });
 });
